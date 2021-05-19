8bc4d2f314abef4843d1552d41539d80f20e52f0
maha <maha@gmail.com>
2021-05-19 07:06:02 +0530

    WebLocators

---
+++ May2021_6AMSeleniumBatch/src/com/launchings/TC_003.java
@@ -0,0 +1,44 @@
+package com.launchings;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("amazonurl");
+		
+		//WebElement loc = driver.findElement(By.id("searchDropdownBox"));
+		//loc.sendKeys("Books");
+		
+		//driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
+		
+		//driver.findElement(By.name("field-keywords")).sendKeys("Harry Potter");
+		
+		//driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		
+		//driver.findElement(By.linkText("Customer Service")).click();
+		
+		//driver.findElement(By.partialLinkText("Customer Serv")).click();
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		for(int i=0;i<links.size();i++)
+		{
+			if(!links.get(i).getText().isEmpty())
+			{
+				System.out.println(links.get(i).getText());
+			}
+			
+		}
+		
+	}
+
+}
