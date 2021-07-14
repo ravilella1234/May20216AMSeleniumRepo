fc194d97eb1aa17e5da68782e82add56b78e4327
maha <maha@gmail.com>
2021-07-14 06:18:37 +0530

    Hybrid Framework Day-3

--- AutomationProject/src/main/java/com/project/keywords/GenericKeywords.java
+++ AutomationProject/src/main/java/com/project/keywords/GenericKeywords.java
@@ -1,8 +1,14 @@
 package com.project.keywords;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -29,17 +35,20 @@ public class GenericKeywords
 	
 	public void navigate(String url)
 	{
-		
+		System.out.println("Navigate to :-" + url);
+		driver.get(url);
 	}
 	
 	public void click(String locator)
 	{
-		
+		//driver.findElement(By.id(locator)).click();
+		getElement(locator).click();
 	}
 	
 	public void type(String locator,String text)
 	{
-		
+		//driver.findElement(By.id(locator)).sendKeys(text);
+		getElement(locator).sendKeys(text);
 	}
 	
 	public void select(String locator,String option)
@@ -50,6 +59,51 @@ public class GenericKeywords
 	public void getText(String locator)
 	{
 		
+	}
+	
+	public WebElement getElement(String locator)
+	{
+		//check the presence of element
+		if(!isElementPresent(locator)) {
+			//report the failure
+			System.out.println("Element is not present :-" + locator);
+		}
+		
+		//check the visibility of element
+		if(!isElementVisible(locator)) {
+			//report the failure
+			System.out.println("Element is not visible :- " + locator);
+		}
+		
+		WebElement e = driver.findElement(By.id(locator));
+		return e;
+	}
+
+	public boolean isElementVisible(String locator) 
+	{
+		System.out.println("Checking the visibility of Element :-" + locator);
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+		
+		try{
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(locator)));
+		}catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean isElementPresent(String locator) 
+	{
+		System.out.println("Checking the Presence of Element :-" + locator);
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		
+		try {
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id(locator)));
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
 	}
 	
 	
--- AutomationProject/src/test/java/com/project/suiteA/CreatePortifolioTest.java
+++ AutomationProject/src/test/java/com/project/suiteA/CreatePortifolioTest.java
@@ -18,6 +18,12 @@ public class CreatePortifolioTest
 	  //ValidationKeywords vKeywords = new ValidationKeywords();
 	  
 	  app.openBrowser("chrome");
+	  app.navigate("https://portfolio.rediff.com/portfolio-login");
+	  app.type("useremail", "ravilella2021@rediff.com");
+	  app.type("userpass", "Sai@2015");
+	  app.click("loginsubmit");
+	  
+	  
 	 // app.navigate("https://www.in.rediff.com");
 	  //app.ValidateTitle();
 	  //app.validateElementPresent();
